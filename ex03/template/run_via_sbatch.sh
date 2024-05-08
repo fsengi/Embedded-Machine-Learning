d7d3a2a83a62c58bb187d0f015937a1cad8cbc91
Florian.Schrittwiese <florian.schrittwieser@web.de>
2024-05-08 16:21:00 +0200

    add ex03 files

---
+++ ex03/template/run_via_sbatch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --gres gpu:1
+#SBATCH --mem 5G
+#SBATCH --cpus-per-task 1
+#SBATCH --time 30:00
+#SBATCH -p exercise-eml
+#SBATCH -o slurm_output.log
+
+# load appropriate conda paths, because we are not in a login shell
+eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
+conda activate eml
+
+echo "Running exercise03_template.py"
+python exercise03_template.py
